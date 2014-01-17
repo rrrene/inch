b2291bf272e6a40b711234d1c1a16c9c3421d40b
René Föhring <rf@bamaru.de>
2014-01-17 17:23:22 +0100

    Add failing tests
    The parsing of .yardopts files and args by YARD influences the command-line
    arguments Inch recognizes.

--- test/inch/cli/yardopts_helper_test.rb
+++ test/inch/cli/yardopts_helper_test.rb
@@ -1,10 +1,10 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../test_helper')
 
-describe ::Inch::CLI::Command::List do
+describe ::Inch::CLI::YardoptsHelper do
   before do
     Dir.chdir fixture_path(:yardopts)
     assert File.file?(".yardopts")
-    @command = ::Inch::CLI::Command::List.new
+    @command = ::Inch::CLI::Command::List
   end
 
   it "should run without args" do
@@ -20,8 +20,20 @@ describe ::Inch::CLI::Command::List do
 
   it "should run with --no-yardopts" do
     out, err = capture_io do
-      @command.run("app/**/*.rb", "--no-yardopts")
+      @instance = @command.run("app/**/*.rb", "--no-yardopts")
     end
+    assert_equal ["app/**/*.rb"], @instance.files
+    assert out.empty?, "there should be no output"
+    assert err.empty?, "there should be no errors"
+  end
+
+  it "should run with a given path and .yardopts" do
+    out, err = capture_io do
+      # lib/something*.rb doesnot exist in the fixture, it is just given as
+      # a command-line path to check if it appears in the parsed filelist
+      @instance = @command.run("lib/something*.rb")
+    end
+    assert_equal ["foo/**/*.rb", "lib/something*.rb"], @instance.files
     assert out.empty?, "there should be no output"
     assert err.empty?, "there should be no errors"
   end
@@ -35,4 +47,28 @@ describe ::Inch::CLI::Command::List do
     #assert_match /\b\-\-\[no\-\]yardopts\b/, out, "--[no-]yardopts should be mentioned"
     assert err.empty?, "there should be no errors"
   end
+end
+
+describe ::Inch::CLI::YardoptsHelper do
+  before do
+    Dir.chdir fixture_path(:simple)
+    refute File.file?(".yardopts")
+    refute File.file?(".document")
+    @command = ::Inch::CLI::Command::List
+  end
+
+  it "should not interfere with paths in arguments" do
+    out, err = capture_io do
+      @instance = @command.run("lib/**/foo*.rb")
+    end
+    assert_equal ["lib/**/foo*.rb"], @instance.files
+  end
+
+  it "should not intefer with --full at the end" do
+    out, err = capture_io do
+      @instance = @command.run("lib/**/foo*.rb", "--full")
+    end
+    assert_equal ["lib/**/foo*.rb"], @instance.files
+  end
+
 end
