0c680816722604a5b67f067d58dab42701c2bdb4
René Föhring <rf@bamaru.de>
2014-01-19 20:20:36 +0100

    Add caching for proxy code objects

--- lib/inch/code_object/proxy.rb
+++ lib/inch/code_object/proxy.rb
@@ -1,16 +1,28 @@
 module Inch
   module CodeObject
     module Proxy
-      def self.for(code_object)
-        class_for(code_object).new(code_object)
-      end
+      class << self
+        def for(code_object)
+          @cache ||= {}
+          if proxy_object = @cache[cache_key(code_object)]
+            proxy_object
+          else
+            @cache[cache_key(code_object)] = class_for(code_object).new(code_object)
+          end
+        end
 
-      private
-      def self.class_for(code_object)
-        class_name = code_object.class.to_s.split('::').last
-        eval("::Inch::CodeObject::Proxy::#{class_name}")
-      rescue
-        Base
+        private
+
+        def class_for(code_object)
+          class_name = code_object.class.to_s.split('::').last
+          eval("::Inch::CodeObject::Proxy::#{class_name}")
+        rescue
+          Base
+        end
+
+        def cache_key(o)
+          o.path
+        end
       end
     end
   end
