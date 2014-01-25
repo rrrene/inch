deacb64b8f88b934003e183dd346a91e2e467008
René Föhring <rf@bamaru.de>
2014-01-25 16:17:38 +0100

    Add fixtures and tests

---
+++ test/fixtures/simple/lib/namespaces.rb
@@ -0,0 +1,46 @@
+
+# @deprecated
+# @see PureNamespace
+PUBLIC_ROOT_CONSTANT = :foo
+
+module Inch
+  module PureNamespace
+  end
+
+  # @deprecated
+  # @see PureNamespace
+  module ManyChildren
+    class Base0; end
+    class Base1; end
+    class Base2; end
+    class Base3; end
+    class Base4; end
+    class Base5; end
+    class Base6; end
+    class Base7; end
+    class Base8; end
+    class Base9; end
+    class Base10; end
+    class Base11; end
+    class Base12; end
+    class Base13; end
+    class Base14; end
+    class Base15; end
+    class Base16; end
+    class Base17; end
+    class Base18; end
+    class Base19; end
+    class Base20; end
+    class Base21; end
+  end
+
+  module ManyAttributes
+    attr_accessor :base0
+    attr_accessor :base1
+    attr_accessor :base2
+    attr_accessor :base3
+    attr_accessor :base4
+    attr_accessor :base5
+    attr_accessor :base6
+  end
+end
---
+++ test/integration/stats_options_test.rb
@@ -0,0 +1,34 @@
+require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
+
+#
+# These format tests also broke things in the regular testsuite.
+# See ./list_options_test.rb
+#
+# Therefore, here are some integration tests:
+#
+describe ::Inch::CLI::Command::List do
+  before do
+    Dir.chdir fixture_path(:simple)
+    @command = "bundle exec inch stats"
+  end
+
+  def assert_parsed_output(parsed)
+    assert parsed.size > 0
+    assert parsed["ranges"]
+    assert parsed["scores"]
+    assert parsed["priorities"]
+  end
+
+  it "should run with --no-private switch" do
+    out = %x|#{@command} --format json|
+    refute out.empty?, "there should be some output"
+    assert_parsed_output JSON[out]
+  end
+
+  it "should run with --no-protected switch" do
+    out = %x|#{@command} --format yaml|
+    refute out.empty?, "there should be some output"
+    assert_parsed_output YAML.load(out)
+  end
+
+end
