28b2d2137e699d00eb1aa71b59cc79d566ccfa2c
René Föhring <rf@bamaru.de>
2014-02-07 02:00:22 +0100

    Refactor some more methods

--- lib/inch/cli/command/output/inspect.rb
+++ lib/inch/cli/command/output/inspect.rb
@@ -30,7 +30,6 @@ module Inch
 
             print_file_info(o)
             print_code_info(o)
-            print_doc_info(o)
             print_namespace_info(o)
             print_roles_info(o)
 
@@ -59,50 +58,29 @@ module Inch
               echo "No roles assigned.".dark
             else
               o.roles.each do |role|
-                name = role.class.to_s.split('::Role::').last
-                if role.potential_score
-                  score = "(#{role.potential_score.to_i})".rjust(5).yellow.dark
-                else
-                  value = role.score.to_i
-                  score = value.abs.to_s.rjust(4)
-                  if value < 0
-                    score = ("-" + score).red
-                  elsif value > 0
-                    score = ("+" + score).green
-                  else
-                    score = " " + score
-                  end
-                end
-                priority = role.priority.to_s.rjust(4)
-                if role.priority == 0
-                  priority = priority.dark
-                end
-                echo name.ljust(40) + score + priority
-                if role.max_score
-                  echo "  (set max score to #{role.max_score})"
-                end
-                if role.min_score
-                  echo "  (set min score to #{role.min_score})"
-                end
+                print_role_info(role)
               end
             end
             echo separator
           end
 
-          def print_doc_info(o)
-            if o.nodoc?
-              echo "The object was tagged not to be documented.".yellow
-            else
-              echo "Docstring".ljust(LJUST) + "#{o.has_doc? ? 'Yes' : 'No text'}"
-              if o.method?
-                echo "Parameters:".ljust(LJUST) + "#{o.has_parameters? ? '' : 'No parameters'}"
-                o.parameters.each do |p|
-                  echo "  " + p.name.ljust(LJUST-2) + "#{p.mentioned? ? 'Mentioned' : 'No text'} / #{p.typed? ? 'Typed' : 'Not typed'} / #{p.described? ? 'Described' : 'Not described'}"
-                end
-                echo "Return type:".ljust(LJUST) + "#{o.return_mentioned? ? 'Defined' : 'Not defined'}"
-              end
+          def print_role(role)
+            name = role.class.to_s.split('::Role::').last
+            score = colored_role_score(role)
+            
+            priority = role.priority.to_s.rjust(4)
+            if role.priority == 0
+              priority = priority.dark
             end
-            echo separator
+            
+            echo name.ljust(40) + score + priority
+
+            if role.max_score
+              echo "  (set max score to #{role.max_score})"
+            end
+            if role.min_score
+              echo "  (set min score to #{role.min_score})"
+            end
           end
 
           def print_namespace_info(o)
@@ -112,6 +90,22 @@ module Inch
                 echo "+ " + child.path.color(COLOR)
               end
               echo separator
+            end
+          end
+
+          def colored_role_score(role)
+            if role.potential_score
+              "(#{role.potential_score.to_i})".rjust(5).yellow.dark
+            else
+              value = role.score.to_i
+              score = value.abs.to_s.rjust(4)
+              if value < 0
+                ("-" + score).red
+              elsif value > 0
+                ("+" + score).green
+              else
+                " " + score
+              end
             end
           end
 
