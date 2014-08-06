0f3d999e5b4d5d642245d0420b3eae61177988e2
ChaYoung You <yousbe@gmail.com>
2014-08-06 16:15:50 +0900

    Make indentation consistent

--- test/unit/code_object/provider/yard/docstring_test.rb
+++ test/unit/code_object/provider/yard/docstring_test.rb
@@ -8,7 +8,7 @@ describe ::Inch::CodeObject::Provider::YARD::Docstring do
   #
 
   it "should notice things in tomdoc style docs" do
-text = <<-DOC
+    text = <<-DOC
 Internal: Detects the Language of the blob.
 
 param1 - String filename
@@ -18,7 +18,7 @@ param2 - String blob data. A block also maybe passed in for lazy
 param3 - Optional String mode (defaults to nil)
 
 Returns Language or nil.
-DOC
+    DOC
     docstring = described_class.new(text)
     assert docstring.describes_internal_api?
     assert docstring.mentions_parameter?(:param1)
@@ -33,7 +33,7 @@ DOC
   end
 
   it "should notice things in tomdoc style docs 2" do
-text = <<-DOC
+    text = <<-DOC
 Public: Look up Language by one of its aliases.
 
 param1 - A String alias of the Language
@@ -44,7 +44,7 @@ Examples
   # => #<Language name="C++">
 
 Returns the Lexer or nil if none was found.
-DOC
+    DOC
     docstring = described_class.new(text)
     assert docstring.mentions_parameter?(:param1)
     assert docstring.describes_parameter?(:param1)
@@ -56,7 +56,7 @@ DOC
   end
 
   it "should notice things in tomdoc style docs 3" do
-text = <<-DOC
+    text = <<-DOC
 Public: Look up Language by one of its aliases.
 
 param1 - A String alias of the Language
@@ -67,7 +67,7 @@ Examples
   # => #<Language name="C++">
 
 Returns the Lexer or nil if none was found.
-DOC
+    DOC
     docstring = described_class.new(text)
     assert docstring.mentions_parameter?(:param1)
     assert docstring.describes_parameter?(:param1)
@@ -79,37 +79,37 @@ DOC
   end
 
   it "should understand 'Returns nil.'" do
-text = <<-DOC
+    text = <<-DOC
 [...]
 Returns nil.
-DOC
+    DOC
     docstring = described_class.new(text)
     assert docstring.describes_return?
   end
 
   it "should understand 'Returns nil.' without fullstop and in lowercase" do
-text = <<-DOC
+    text = <<-DOC
 [...]
 returns nil
-DOC
+    DOC
     docstring = described_class.new(text)
     assert docstring.describes_return?
   end
 
   it "should understand 'Returns nothing.'" do
-text = <<-DOC
+    text = <<-DOC
 [...]
 Returns nothing.
-DOC
+    DOC
     docstring = described_class.new(text)
     assert docstring.describes_return?
   end
 
   it "should understand 'Returns nothing.' without fullstop and in lowercase" do
-text = <<-DOC
+    text = <<-DOC
 [...]
 returns nothing
-DOC
+    DOC
     docstring = described_class.new(text)
     assert docstring.describes_return?
   end
@@ -126,20 +126,20 @@ DOC
   #
 
   it "should work 2" do
-text = <<-DOC
+    text = <<-DOC
 Just because format_html is mentioned here, does not mean
 the first parameter is mentioned.
-DOC
+    DOC
     docstring = described_class.new(text)
     refute docstring.mentions_parameter?(:format)
     refute docstring.contains_code_example?
   end
 
   it "should work 2 if correct" do
-text = <<-DOC
+    text = <<-DOC
 Just because format is mentioned here, does not mean
 the first parameter is meant.
-DOC
+    DOC
     docstring = described_class.new(text)
     refute docstring.mentions_parameter?(:format)
     refute docstring.contains_code_example?
@@ -150,7 +150,7 @@ DOC
   #
 
   it "should work 3" do
-text = <<-DOC
+    text = <<-DOC
 An example of a method using RDoc rather than YARD.
 
 == Parameters:
@@ -159,13 +159,13 @@ param1::
 
 == Returns:
 A string in the specified format.
-DOC
+    DOC
     docstring = described_class.new(text)
     refute docstring.contains_code_example?
   end
 
   it "should work with code example" do
-text = <<-DOC
+    text = <<-DOC
 Another example.
 
   method_with_code_example() # => some value
@@ -174,7 +174,7 @@ Params:
 +param1+:: param1 line string to be executed by the system
 +param2+:: +Proc+ object that takes a pipe object as first and only param (may be nil)
 +param3+:: +Proc+ object that takes a pipe object as first and only param (may be nil)
-DOC
+    DOC
     docstring = described_class.new(text)
     assert docstring.contains_code_example?
     assert docstring.mentions_parameter?(:param1)
@@ -186,20 +186,20 @@ DOC
   end
 
   it "should work with code example 2" do
-text = <<-DOC
+    text = <<-DOC
 Just because format_html is mentioned here, does not mean
 the first parameter is mentioned.
 
   method_with_code_example() # => some value
   method_with_missing_param_doc(param1, param2, param3)
-DOC
+    DOC
     docstring = described_class.new(text)
     assert docstring.contains_code_example?
     assert_equal 1, docstring.code_examples.size
   end
 
   it "should work with code example 3" do
-text = <<-DOC
+    text = <<-DOC
 An example of a method using RDoc rather than YARD.
 
   method_with_code_example() # => some value
@@ -210,7 +210,7 @@ param1::
 
 == Returns:
 A string in the specified format.
-DOC
+    DOC
     docstring = described_class.new(text)
     assert docstring.contains_code_example?
     assert_equal 1, docstring.code_examples.size
@@ -219,7 +219,7 @@ DOC
   end
 
   it "should work with multiple code examples" do
-text = <<-DOC
+    text = <<-DOC
 An example of a method using RDoc rather than YARD.
 
   method_with_code_example() # => some value
@@ -235,7 +235,7 @@ param1::
 
 == Returns:
 A string in the specified format.
-DOC
+    DOC
     docstring = described_class.new(text)
     assert docstring.contains_code_example?
     assert_equal 2, docstring.code_examples.size
