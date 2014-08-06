0022419cb04c99781cae7c485ef490ca8957a6a5
ChaYoung You <yousbe@gmail.com>
2014-08-06 17:05:23 +0900

    Add space inside hash literal braces

--- test/unit/api/options/base_test.rb
+++ test/unit/api/options/base_test.rb
@@ -8,7 +8,7 @@ describe ::Inch::API::Options::Base do
   end
 
   it "should work with a Hash or Struct" do
-    @options_hash = {foo: "foo", baz: 42}
+    @options_hash = { foo: "foo", baz: 42 }
     @options_struct = OpenStruct.new(@options_hash)
 
     @options1 = APIOptionsTest.new @options_hash
@@ -21,7 +21,7 @@ describe ::Inch::API::Options::Base do
   end
 
   it "should return default values" do
-    @options_hash = {baz: 42}
+    @options_hash = { baz: 42 }
     @options = APIOptionsTest.new @options_hash
 
     assert_equal :bar, @options.foo
